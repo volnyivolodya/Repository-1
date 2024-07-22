84b45cc5d9150eaa56b5cc5ac6d94d451b7833a0
Vlagimir Ivanov <volnyivolodya@gmail.com>
2024-07-22 13:29:25 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,106 @@
+from flask import Flask, request
+from marshmallow import ValidationError
+
+from eshop.businsess_logic.order_usecases import order_create, order_get_many, order_get_by_id
+from eshop.businsess_logic.product_usecases import product_create, product_get_many, product_get_by_id
+from eshop.view.order_schemas import OrderCreateDtoSchema, OrderSchema, OrderGetManyParams
+from eshop.view.product_schemas import ProductCreateDtoSchema, ProductSchema, ProductGetManyParams
+
+app = Flask(__name__)
+
+
+@app.post("/api/v1/order")
+def order_create_endpoint():
+    try:
+        order_create_dto = OrderCreateDtoSchema().load(request.json)
+    except ValidationError as err:
+        return err.messages, 400
+
+    try:
+        order = order_create(
+            product_ids=order_create_dto['product_ids']
+        )
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
+
+    return OrderSchema().dump(order)
+
+
+@app.post("/api/v1/product")
+def product_create_endpoint():
+    try:
+        product_create_dto = ProductCreateDtoSchema().load(request.json)
+    except ValidationError as err:
+        return err.messages, 400
+
+    try:
+        product = product_create(
+            name=product_create_dto['name'],
+            price=product_create_dto['price']
+        )
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
+
+    return ProductSchema().dump(product)
+
+
+@app.get("/api/v1/order")
+def order_get_many_endpoint():
+    try:
+        params = OrderGetManyParams().load(request.args)
+    except ValidationError as err:
+        return err.messages, 400
+
+    orders = order_get_many(
+        page=params['page'],
+        limit=params['limit'],
+    )
+
+    return OrderSchema(many=True).dump(orders)
+
+
+@app.get("/api/v1/product")
+def product_get_many_endpoint():
+    try:
+        product_get_many_params = ProductGetManyParams().load(request.args)
+    except ValidationError as err:
+        return err.messages, 400
+
+    product = product_get_many(
+        page=product_get_many_params['page'],
+        limit=product_get_many_params['limit'],
+    )
+
+    return ProductSchema(many=True).dump(product)
+
+
+@app.get("/api/v1/order/<id>")
+def order_get_by_id_endpoint(id):
+    order = order_get_by_id(id)
+
+    if order is None:
+        return {
+            "error": 'Not found'
+        }, 404
+
+    return OrderSchema().dump(order)
+
+
+@app.get("/api/v1/product/<id>")
+def product_get_by_id_endpoint(id):
+    product = product_get_by_id(id)
+
+    if product is None:
+        return {
+            "error": 'Not found'
+        }, 404
+
+    return ProductSchema().dump(product)
+
+
+def run_server():
+    app.run()
